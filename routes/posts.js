d8202aeb4cb984280c855928e584fef75fd28be0
00009987 <00009987js@gmail.com>
2021-03-20 16:52:26 +0500

    add edit functionality

--- routes/posts.js
+++ routes/posts.js
@@ -57,4 +57,62 @@ router.get('/post_:id/delete', (req, res) => {
 	res.redirect('/posts');
 });
 
+router.get('/post_:id/edit', (req, res) => {
+	let db = [];
+
+	fs.readFile(
+		path.join(__dirname, '..', '/database/db.json'),
+		'utf-8',
+		(err, data) => {
+			if (err) throw err;
+			else db = JSON.parse(data);
+
+			let post = db.filter((post) => post.id === req.params.id)[0];
+
+			res.render('edit', { post });
+		},
+	);
+});
+
+router.post('/post_:id/edit', (req, res) => {
+	const currentDate = require('../actions/getCurrentDate');
+
+	const blog = {
+		id: req.params.id,
+		title: req.body.title,
+		description: req.body.description,
+		author: req.body.author,
+		post: req.body.post,
+		date: currentDate,
+	};
+
+	let db = [];
+
+	fs.readFile(
+		path.join(__dirname, '..', '/database/db.json'),
+		'utf-8',
+		(err, data) => {
+			if (err) throw err;
+			else db = JSON.parse(data);
+
+			let currentPost = db.filter((post) => post.id === req.params.id)[0];
+			let postIndex = db.indexOf(currentPost);
+			db[postIndex] = blog;
+
+			fs.writeFile(
+				path.join(__dirname, '..', '/database/db.json'),
+				JSON.stringify(db),
+				(err) => {
+					if (err) throw err;
+				},
+			);
+		},
+	);
+
+	res.redirect('/posts');
+});
+
 module.exports = router;
+
+// random post - icon button
+// search & sort
