be296cb161bac296a52f5696203e773fdb01a210
00009987 <00009987js@gmail.com>
2021-03-27 19:39:57 +0500

    update LightDarkMode.js

--- public/scripts/LightDarkMode.js
+++ public/scripts/LightDarkMode.js
@@ -5,9 +5,9 @@ const body = document.body;
 // Event Listeners
 document.addEventListener('DOMContentLoaded', () => {
 	// setting theme when webpage loads
-	let theme = checkStorage();
-	if (theme) {
-		body.classList.add(theme);
+	let themes = checkStorage();
+	if (themes) {
+		body.classList.add(themes[0]);
 	}
 });
 
@@ -23,9 +23,9 @@ function checkStorage() {
 	// check if the mode is already changed
 	let themes;
 	if (localStorage.getItem('themes') === null) {
-		themes = '';
+		themes = [];
 	} else {
 		themes = JSON.parse(localStorage.getItem('themes'));
 	}
-	return themes[0];
+	return themes;
 }
